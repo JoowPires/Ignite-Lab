53478a509e063ca7de0d2b0f7042ec2fbcfa63b9
joowpires <jonathanpiresneves@hotmail.com>
2022-06-24 18:29:42 -0300

    Inscrição via GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4on3nmw0jza01z4hjmm6gvw/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
